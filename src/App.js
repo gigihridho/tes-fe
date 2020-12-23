262c1d3b2ae6d9f466f02bddad63d556f27a0b50
gigihridho <gigih.r.r@mail.ugm.ac.id>
2020-12-24 01:03:59 +0700

    repair view

--- src/App.js
+++ src/App.js
@@ -7,39 +7,51 @@ const SEARCH_API = "https://api.themoviedb.org/3/search/movie&?api_key=aa6eb3d22
 function App() {
 
     const [movies, setMovies] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
+    const [searchTerm, setSearchTerm] = useState("");
 
     useEffect( () => {
-      fetch(FEATURED_API)
-        .then(res => res.json())
-        .then(data => {
-          setMovies(data.results);
+      getMovies(FEATURED_API);
+    }, []);
+
+    const getMovies = (API) => {
+      fetch(API)
+        .then((res) => res.json())
+        .then((data) => {
+            setMovies(data.results);
       });
-    }, [])
+    };
 
     const handleOnSubmit = (e) => {
       e.preventDefault();
+
+      if(searchTerm){
+        getMovies(SEARCH_API + searchTerm);
+        setSearchTerm("");
+      }
     };
 
     const handleOnChange = (e) => {
       setSearchTerm(e.target.value);
-    }
+    };
+
     return (
       <>
         <header>
           <form onSubmit={handleOnSubmit}>
             <input 
-            type="text" 
-            placeholder="Search..." 
-            className="search"
-            value={searchTerm}
-            onChange={handleOnChange}/>
+              type="search" 
+              placeholder="Search..." 
+              className="search"
+              value={searchTerm}
+              onChange={handleOnChange}
+            />
           </form>
         </header>
         <div className="movie-container">
-          {movies.length > 0 && movies.map(movie => (
-            <Movie key={movie.id} {...movie} />
-          ))}
+          {movies.length > 0 && 
+            movies.map((movie) => <Movie 
+              key={movie.id} {...movie} />
+          )}
         </div>
       </>
     );
