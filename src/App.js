dda8d3912f7bd4cdd0dfcd79fce8d7bb0f81644b
gigihridho <gigih.r.r@mail.ugm.ac.id>
2020-12-24 00:32:53 +0700

    add search

--- src/App.js
+++ src/App.js
@@ -7,22 +7,41 @@ const SEARCH_API = "https://api.themoviedb.org/3/search/movie&?api_key=aa6eb3d22
 function App() {
 
     const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     useEffect( () => {
       fetch(FEATURED_API)
         .then(res => res.json())
         .then(data => {
-          console.log(data);
           setMovies(data.results);
       });
     }, [])
 
+    const handleOnSubmit = (e) => {
+      e.preventDefault();
+    };
+
+    const handleOnChange = (e) => {
+      setSearchTerm(e.target.value);
+    }
     return (
-    <div className="movie-container">
-      {movies.length > 0 && movies.map(movie => (
-        <Movie key={movie.id} {...movie} />
-      ))}
-    </div>
+      <>
+        <header>
+          <form onSubmit={handleOnSubmit}>
+            <input 
+            type="text" 
+            placeholder="Search..." 
+            className="search"
+            value={searchTerm}
+            onChange={handleOnChange}/>
+          </form>
+        </header>
+        <div className="movie-container">
+          {movies.length > 0 && movies.map(movie => (
+            <Movie key={movie.id} {...movie} />
+          ))}
+        </div>
+      </>
     );
 }
 
